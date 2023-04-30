ab224a92840eb8b8961392bf52456d21991c57e7
enzoroh1 <cave_enzo_96@hotmail.com>
2023-04-29 21:51:01 -0300

    arreglos de CSS

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   const inicio = useRef();
   const app = useRef();
   const { theme } = useTheme();
+  //Nuevo comentario mas este
 
   // useEffect(() => {
   //   if (theme === "dark") {
